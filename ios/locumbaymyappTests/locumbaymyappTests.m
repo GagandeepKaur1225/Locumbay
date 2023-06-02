b3e3a05823e44bd7d2cf8635d40d328d965b65f3
gaganDeep <gagandeep.kaur@chicmic.co.in>
2023-06-02 09:58:44 +0530

    feat:facebook login

--- ios/locumbaymyappTests/locumbaymyappTests.m
+++ ios/locumbaymyappTests/locumbaymyappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface LocumbayTests : XCTestCase
+@interface locumbaymyappTests : XCTestCase
 
 @end
 
-@implementation LocumbayTests
+@implementation locumbaymyappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
